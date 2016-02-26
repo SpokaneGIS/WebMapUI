9137f5551afc0b339ff1419feb396f8e89c2e384
Bill Myers <wmyers@spokanecity.org>
2016-02-25 16:28:07 -0800

    Initial Commit

---
+++ _script/map1.0.0.js
@@ -0,0 +1,21 @@
+﻿//-------------------------------------//
+//Config
+//-------------------------------------//
+//namespace
+var cosgis = {};
+
+//map
+cosgis.map = null;
+cosgis.mapCenter = [47.675, -117.41];
+
+//-------------------------------------//
+//Map Init
+//-------------------------------------//
+
+//Map setup
+cosgis.map = L.map('map', { zoomControl: false }).setView(cosgis.mapCenter, 12);
+cosgis.basemap = L.esri.basemapLayer('Topographic', { detectRetina: true }).addTo(cosgis.map);
+
+//Controls
+L.control.zoom({ position: 'bottomright' }).addTo(cosgis.map);
+L.control.scale().addTo(cosgis.map);
