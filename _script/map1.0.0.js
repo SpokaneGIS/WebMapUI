15f1533c4d190eceb038ac5c9a856bb7e406ca3e
Bill Myers <wmyers@spokanecity.org>
2016-03-01 17:29:32 -0800

    3/1/2016 update 3

--- _script/map1.0.0.js
+++ _script/map1.0.0.js
@@ -20,21 +20,53 @@ cosgis.basemap = L.esri.basemapLayer('Topographic', { detectRetina: true }).addT
 L.control.zoom({ position: 'bottomright' }).addTo(cosgis.map);
 L.control.scale().addTo(cosgis.map);
 
+//Disbale map interaction in left panel
+$('.container').on('mouseover', function () {
+    cosgis.map.dragging.disable();
+});
+$('.container').on('mouseout', function () {
+    cosgis.map.dragging.enable();
+});
+
+//Disable map click propagtion in content panels
+disableMapClick();
+
 //-------------------------------------//
 //Methods
 //-------------------------------------//
 
 //UI
-function fade(Id) {
-    $(Id).fadeToggle("fast", "linear");
+function fade(id) {
+    $(id).fadeToggle("fast", "linear");
 }
 
-function togglePanel(panelID) {
-    $(panelID).toggle('slide', {}, 400);
+function togglePanel(panelID, slideDirection) {
+    $(panelID).toggle('slide', { direction: slideDirection }, 300);
+}
+
+function hidePanel(panelID, slideDirection) {
+    $(panelID).hide('slide', { direction: slideDirection }, 300);
+}
+
+function showPanel(panelID, slideDirection) {
+    $(panelID).show('slide', { direction: slideDirection }, 300);
 }
 
 function toggleMenu() {
     fade('#mapDisabled');
-    fade('#divLeft');
+    togglePanel('#divLeft', 'left');
+}
+
+function disableMapClick() {
+    var divs = ['divMenuBar', 'divLeft', 'divRight', 'mapDisabled'];
+    for (var i = 0; i < divs.length; i++) {
+        var divPanel = L.DomUtil.get(divs[i]);
+        if (!L.Browser.touch) {
+            L.DomEvent.disableClickPropagation(divPanel);
+            L.DomEvent.on(divPanel, 'mousewheel', L.DomEvent.stopPropagation);
+        } else {
+            L.DomEvent.on(divPanel, 'click', L.DomEvent.stopPropagation);
+        }
+    }
 }
 
